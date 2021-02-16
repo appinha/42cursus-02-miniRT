1c48ed8e3a72dfeef3ce16ac1c397269d07c93d1
appinha <amanda_pinha@hotmail.com>
2021-02-16 13:51:03 -0300

    Corrected bug of GNL not reading last line of .rt

--- srcs/scene/scene.c
+++ srcs/scene/scene.c
@@ -6,7 +6,7 @@
 /*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 12:17:20 by appinha           #+#    #+#             */
-/*   Updated: 2021/02/14 20:12:31 by apuchill         ###   ########.fr       */
+/*   Updated: 2021/02/15 16:35:09 by apuchill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,11 @@ static void	scene_line_split(t_scene *scene, short int elem_id,
 		error_msg_and_exit(get_error_code(1, elem_id, err_id));
 }
 
-static void	get_scene_elem(t_scene *scene, short int elem_id,
-							void (*ft)(t_scene *, t_elem **))
+static void	get_scene_elem(t_scene *scene, short int elem_id)
 {
-	t_elem	*new;
+	static t_arr_sc	ft_scene[9] = {get_0_resol, get_1_amb_li, get_2_cam,
+				get_3_light, get_4_sp, get_5_pl, get_6_sq, get_7_cy, get_8_tr};
+	t_elem			*new;
 
 	new = NULL;
 	if (elem_id < 2)
@@ -57,7 +58,7 @@ static void	get_scene_elem(t_scene *scene, short int elem_id,
 		new->next = NULL;
 	}
 	scene_line_split(scene, elem_id, 1);
-	(*ft)(scene, &new);
+	ft_scene[elem_id](scene, &new);
 	if (elem_id >= 3)
 		lstadd_back_elem(&scene->light + elem_id - 3, new,
 							&scene->qtys[elem_id]);
@@ -67,25 +68,25 @@ static void	get_scene_elem(t_scene *scene, short int elem_id,
 
 void		init_scene(char *file, t_scene *scene)
 {
-	static t_arr_sc	ft_scene[9] = {get_0_resol, get_1_amb_li, get_2_cam,
-				get_3_light, get_4_sp, get_5_pl, get_6_sq, get_7_cy, get_8_tr};
-	static char		*elem[9] = {"R ", "A ", "c ", "l ",
-								"sp", "pl", "sq", "cy", "tr"};
-	int				id;
-	int				fd;
+	static char	*s[9] = {"R ", "A ", "c ", "l ", "sp", "pl", "sq", "cy", "tr"};
+	int			elem_id;
+	int			fd;
+	int			ret;
 
 	if (ft_strnrcmp(file, ".rt", 3) != 0)
 		error_msg_and_exit("010");
 	*scene = (t_scene) {0};
 	fd = open_ver(file);
-	while (get_next_line(fd, &scene->line) == 1)
+	while ((ret = get_next_line(fd, &scene->line)) >= 0)
 	{
-		id = 0;
-		while (id < 9 && ft_strncmp(scene->line, elem[id], 2))
-			id++;
-		if (id < 9)
-			get_scene_elem(scene, id, *ft_scene[id]);
+		elem_id = 0;
+		while (elem_id < 9 && ft_strncmp(scene->line, s[elem_id], 2))
+			elem_id++;
+		if (elem_id < 9)
+			get_scene_elem(scene, elem_id);
 		free(scene->line);
+		if (ret == 0)
+			break ;
 	}
 	close_ver(fd);
 	if (scene->qtys[0] == 0 || scene->qtys[1] == 0 || scene->qtys[2] == 0)
